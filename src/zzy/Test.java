e6c666df9b91e275ceb2a56c04a354b384aee224
zhaozhenyu <zhaozhenyu_17@163.com>
2018-07-27 22:19:56 +0800

    test

---
+++ src/zzy/Test.java
@@ -0,0 +1,17 @@
+package zzy;
+
+/**
+ * created in 19:09 2018/7/27 by zhaozhenyu.
+ */
+
+public class Test {
+    public static void m() {
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        Test t = null;
+        //t.m();
+        m();
+    }
+}
