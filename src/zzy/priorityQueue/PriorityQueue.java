2f68ec28d33f7aba048ffb295fc3591ab621f7bf
zhaozhenyu <zhaozhenyu_17@163.com>
2018-08-01 00:29:44 +0800

    add priorityQueue

---
+++ src/zzy/priorityQueue/PriorityQueue.java
@@ -0,0 +1,46 @@
+package zzy.priorityQueue;
+
+import zzy.heap.MaxHeap;
+import zzy.queue.Queue;
+
+/**
+ * created in 22:32 2018/7/31 by zhaozhenyu.
+ */
+
+/**
+ * 根据最大堆实现优先队列
+ * @param <E>
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue() {
+        maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return maxHeap.extractMax();
+    }
+
+    @Override
+    public E getFront() {
+        return maxHeap.findMax();
+    }
+
+    @Override
+    public int getSize() {
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return maxHeap.isEmpty();
+    }
+}
